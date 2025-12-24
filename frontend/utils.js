ce832fcdf2c1eed1aac1358ccaa3bbf04b0dd696
Nitesh Nair <niteshnair@Niteshs-MacBook-Air.local>
2025-12-24 10:23:30 +0530

    date correction

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,5 +1,7 @@
 function todayCollectionName() {
-  const d = new Date();
+  const d = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
   const y = d.getFullYear();
   const m = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
